a53c9111d7296737a491fb9f765f43767c2573ad
Asa Tirsen <asa.tirsen@gmail.com>
2020-04-19 15:48:20 +1000

    completed version

--- router/200_dice.php
+++ router/200_dice.php
@@ -24,36 +24,30 @@ $app->router->get("dice/dice_view", function () use ($app) {
     $game = $_SESSION["game"];
     $title = "Play the game";
 
-//    // Get the current settings from session
-//    $score = $_SESSION["score"] ?? null;
-//    $save = $_SESSION["save"] ?? null;
-//    $number = $_SESSION["number"] ?? null;
-//    $doCheat = $_SESSION["doCheat"] ?? null;
-//
-//
-//    $_SESSION["res"] = null;
-//    $_SESSION["guess"] = null;
-//
-//
     $data = [
         "game" => $game,
     ];
     $app->page->add("dice/dice_view", $data);
-////    $app->page->add("guess/debug");
-//
-//
+    //$app->page->add("dice/debug");
+
     return $app->page->render([
         "title" => $title,
     ]);
 });
-
+/**
+ * Make a roll, save the turn or reset the game
+ */
 $app->router->post("dice/dice_view", function () use ($app) {
     $game = $_SESSION["game"];
-    $roll = $_POST["roll"] ?? null;
+    $humanroll = $_POST["humanroll"] ?? null;
+    $computerroll = $_POST["computerroll"] ?? null;
     $save = $_POST["save"] ?? null;
     $reset = $_POST["reset"] ?? null;
-    if ($roll) {
+    if ($humanroll) {
         $game->humanRoll();
+    }
+    if ($computerroll) {
+        $game->computerRoll();
     }
     if ($save) {
         $game->humanSave();
@@ -64,78 +58,3 @@ $app->router->post("dice/dice_view", function () use ($app) {
     $_SESSION["game"] = $game;
     return $app->response->redirect("dice/dice_view");
 });
-
-
-//
-//
-///**
-// * Play the game - make a guess
-// */
-$app->router->post("dice/dice_view", function () use ($app) {
-    $game = $_SESSION["game"];
-//    // Deal with incoming variables
-//    $guess = $_POST["guess"] ?? null;
-//    $doInit = $_POST["doInit"] ?? null;
-//    $doGuess = $_POST["doGuess"] ?? null;
-//    $doCheat = $_POST["doCheat"] ?? null;
-//
-//    $number = $_SESSION["number"] ?? null;
-//    $tries = $_SESSION["tries"] ?? null;
-//
-//    $_SESSION["doCheat"] = $doCheat;
-//
-//    if ($doInit) {
-//        // Init the game;
-//        $game = new Asatir\Guess\GuessB();
-//        $_SESSION["number"] = $game->number();
-//        $_SESSION["tries"] = $game->tries();
-//    }
-//
-//    if ($doGuess) {
-//        error_log('doGuess');
-//        //Do a guess
-//        $game = new Asatir\Guess\GuessB($number, $tries);
-//        $res = $game->makeGuess($guess);
-//        $_SESSION["tries"] = $game->tries();
-//        $_SESSION["res"] = $res;
-//        $_SESSION["guess"] = $guess;
-//
-//        if ($_SESSION["tries"] < 0) {
-//            $game = new Asatir\Guess\GuessB();
-//            $_SESSION["number"] = $game->number();
-//            $_SESSION["tries"] = $game->tries();
-//        }
-//    }
-
-    $_SESSION["game"] = $game;
-    return $app->response->redirect("dice/dice_view");
-});
-
-//
-//$dice = new Dice();
-//$counter = 1;
-//$diceArray = array();
-//$resArray = array();
-//while ($counter <= 6) {
-//    $res = $dice->roll();
-//    array_push($diceArray, $res);
-//    $counter++;
-//}
-//
-//foreach ($diceArray as $key => $res) {
-//    $key = $key+1;
-//    print_r("<li>$key . $res</li><br>");
-//}
-//$arraySum = array_sum($diceArray);
-//
-//print_r("<p>Summan är: $arraySum<br></p>");
-//
-//$arrayAve = array_sum($diceArray)/count($diceArray);
-//
-//$roundAve = round($arrayAve, 2);
-//
-//print_r ("<p>Medelvärdet är: $roundAve</p>");
-//
-//
-//$hand = new DiceHand();
-//$hand->roll();
